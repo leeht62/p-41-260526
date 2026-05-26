3ba887d478f159a98afbe1e994ec944b4a17673d
leeht62 <zdfxd3611@naver.com>
2026-05-26 23:00:57 +0900

    init

---
+++ src/main/kotlin/com/back/HomeController.kt
@@ -0,0 +1,25 @@
+package com.back
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import software.amazon.awssdk.services.s3.S3Client
+
+@RestController
+class HomeController(
+    private val s3Client: S3Client,
+    @Value("\${custom.secretWord}")
+private val secretWord: String = ""
+) {
+
+    @GetMapping
+    fun main(): String {
+        return "Hi, $secretWord"
+    }
+
+    @GetMapping("/buckets")
+    fun buckets(): List<String> {
+        return s3Client.listBuckets().buckets().map { it.name() }
+    }
+
+}
